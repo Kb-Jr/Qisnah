fe3c39e8633564307f1ff95c28cd9f9991e6f6bc
Kb-Jr <abdulkabiruj@gmail.com>
2023-01-20 20:03:05 +0100

    second commit

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ window.addEventListener('load', function(){
       
     })
   })
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll ('.nav-links a');
+
+    
+    burger.addEventListener('click', () => {
+        // toggle nav
+        nav.classList.toggle('nav-active')
+
+        // Animate links
+        navLinks.forEach((link, index) =>{
+            if (link.style.animation){
+                link.style.animation='';
+            }
+            else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`
+            }
+
+        });
+
+        // burger animation
+        burger.classList.toggle('toggle');
+
+    });
+
+    
+}
+
+navSlide()
